f7b1e19f1e42aadcfd6da628e643d3b69b6892bc
n1ved <nivedrsalini@gmail.com>
2025-05-30 20:52:13 +0530

    feat : rewrite check logic and task interface

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,14 +3,16 @@ import styles from "./page.module.css";
 import dummyProgressData from "@/data/DummyData/progress";
 import dummyCoursesData from "@/data/DummyData/courses";
 import dummyTaskData from "@/data/DummyData/tasks";
-import React from "react";
+import React, {useEffect} from "react";
 import ProgressCard from "@/components/progress/ProgressCard";
 import TaskCheckBox from "@/components/checkbox/TaskCheckBox";
+import {ChevronRight , ChevronDown} from 'lucide-react'
 interface Task {
     id: string,
     name: string,
     course: string,
     date: string,
+    isDone: boolean,
     timeRequired: number,
 }
 interface courseMappedTasks {
@@ -22,34 +24,37 @@ export default function Dashboard() {
   const coursesData = dummyCoursesData();
 
   const [taskData, setTaskData] = React.useState(dummyTaskData());
-  const [doneTasks, setDoneTasks] = React.useState(taskData.done);
-  const [todoTasks, setTodoTasks] = React.useState(taskData.todo);
+  const [doneTasks, setDoneTasks] = React.useState(taskData.filter(task => task.isDone));
+  const [todoTasks, setTodoTasks] = React.useState(taskData.filter(task => !task.isDone));
+  const [scaffoldDone , setScaffoldDone] = React.useState(false);
+  const [mappedTasks , setMappedTasks] = React.useState(coursesData.map(course => {
+      return {
+          course:course,
+          tasks: taskData.filter(task => task.course === course && !task.isDone),
+      }
+  }))
 
 
-  const mappedTasks: Array<courseMappedTasks> = coursesData.map(course => {
-    return {
-      course: course,
-      tasks: taskData.todo.filter(task => task.course === course)
-    }
-  });
+
+
+  useEffect(() => {
+      setDoneTasks(taskData.filter(task => task.isDone));
+      setTodoTasks(taskData.filter(task => !task.isDone));
+      setMappedTasks(coursesData.map(course => {
+          return {
+              course:course,
+              tasks: taskData.filter(task => task.course === course && !task.isDone),
+          }
+      }))
+  },[taskData]);
   
 
   //TODO:This function is a mess
   const handleTaskStatusChange = (id:string,value:boolean) => {
     console.debug(`Task with id ${id} is now ${value ? 'done' : 'not done'}`);
-    if(value){
-        const changedTask = todoTasks.find(task => task.id === id);
-        if(changedTask){
-            setDoneTasks([...doneTasks,changedTask]);
-            setTodoTasks(todoTasks.filter(task => task.id !== id));
-        }
-    } else {
-      const changedTask = doneTasks.find(task => task.id === id);
-      if (changedTask) {
-        setTodoTasks([...todoTasks, changedTask]);
-        setDoneTasks(doneTasks.filter(task => task.id !== id));
-      }
-    }
+    setTaskData(prevState => prevState.map(
+        task => task.id === id ? {...task , isDone: value} : task
+    ))
   }
 
   return (
@@ -62,16 +67,24 @@ export default function Dashboard() {
                 <h2>Checklist</h2>
                 <div className={styles.checklist_cards}>
                     {mappedTasks.map((course) => (
-                        <div key={course.course} className={styles.checklist_card}>
-                            <h3>{course.course}</h3>
-                            {course.tasks.map((task) => (
-                                <TaskCheckBox key={task.name} {...task} isDone={false} handlerFunction={handleTaskStatusChange}/>
+                        course.tasks.length > 0 && (
+                            <div key={course.course} className={styles.checklist_card}>
+                                <h3 className={styles.checklist_course_title}>{course.course}</h3>
+                                {course.tasks.map((task) => (
+                                    <TaskCheckBox key={task.name} {...task} isDone={task.isDone} handlerFunction={handleTaskStatusChange}/>
+                                ))}
+                            </div>
+                        )
+                    ))}
+                    <h3 className={styles.checklist_course_title} onClick={() => {setScaffoldDone(!scaffoldDone)}}> {scaffoldDone ? <ChevronDown/> : <ChevronRight/>} Completed Tasks</h3>
+                    {
+                        scaffoldDone &&
+                        <div className={styles.checklist_cards_done}>
+                            {doneTasks.map((task,index) => (
+                                <TaskCheckBox key={task.name} {...task} isDone={task.isDone} handlerFunction={handleTaskStatusChange}/>
                             ))}
                         </div>
-                    ))}
-                    {doneTasks.map((task,index) => (
-                        <TaskCheckBox key={task.name} {...task} isDone={true} handlerFunction={handleTaskStatusChange}/>
-                  ))}
+                    }
                 </div>
               </section>
               <section className={styles.progress_container}>
